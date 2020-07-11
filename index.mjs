421ea10387ca3fe65936e365744e05009b3522ac
mono <mono@mono0x.net>
2020-07-12 01:29:05 +0900

    Initial commit

---
+++ index.mjs
@@ -0,0 +1,60 @@
+import fs from "fs/promises"
+import path from "path"
+import os from "os"
+import puppeteer from "puppeteer"
+import Twitter from "twitter-lite"
+import format from "date-fns-tz/format/index.js"
+
+(async () => {
+    const urls = [
+        "https://yurugp2020.mono0x.net/charts/LOCAL",
+        "https://yurugp2020.mono0x.net/charts/COMPANY",
+    ]
+
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.setViewport({
+        width: 512,
+        height: 512,
+        deviceScaleFactor: 2,
+    })
+
+    const uploadClient = new Twitter({
+        subdomain: "upload",
+        consumer_key: process.env.TWITTER_CONSUMER_KEY,
+        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+        access_token_key: process.env.TWITTER_OAUTH_TOKEN,
+        access_token_secret: process.env.TWITTER_OAUTH_SECRET,
+    })
+
+    const mediaIds = []
+    for (let i = 0; i < urls.length; i++) {
+        const url = urls[i]
+        const imagePath = path.join(os.tmpdir(), `${i}.png`)
+        await page.goto(url, { waitUntil: "networkidle0" })
+        await page.screenshot({ imagePath })
+        const image = await fs.readFile(imagePath, "base64")
+        const media = await uploadClient.post("media/upload", {
+            media_data: image,
+        })
+        mediaIds.push(media.media_id_string)
+    }
+
+    await browser.close()
+
+    const client = new Twitter({
+        consumer_key: process.env.TWITTER_CONSUMER_KEY,
+        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+        access_token_key: process.env.TWITTER_OAUTH_TOKEN,
+        access_token_secret: process.env.TWITTER_OAUTH_SECRET,
+    })
+
+    const date = format(new Date(), "M月d日", { timeZone: "Asia/Tokyo" })
+    const status = `#ゆるキャラグランプリ2020 ${date}現在\n\n詳細な投票状況はこちら: https://yurugp2020.mono0x.net/\n投票はこちら: https://www.yurugp.jp/jp/vote/`
+    console.log(status)
+
+    await client.post("statuses/update", {
+        status: "hello",
+        media_ids: mediaIds.join(","),
+    })
+})()
