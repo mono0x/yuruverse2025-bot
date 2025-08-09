dcea910e9b8b30efabeea1eb9a42beb863f302ba
mono <mono@mono0x.net>
2025-08-09 12:43:31 +0900

    Introduce Biome

--- index.ts
+++ index.ts
@@ -1,113 +1,128 @@
-import * as path from "path"
-import * as os from "os"
-import * as util from "util"
+import * as os from "node:os"
+import * as path from "node:path"
+import * as util from "node:util"
+import { format } from "date-fns-tz/format"
 import puppeteer, { type Page } from "puppeteer"
 import { TwitterApi } from "twitter-api-v2"
-import { format } from "date-fns-tz/format"
 
 type Rule = {
-    viewport: {
-        width: number
-        height: number
-        deviceScaleFactor: number
-    }
-    url: string
+  viewport: {
+    width: number
+    height: number
+    deviceScaleFactor: number
+  }
+  url: string
 }
 
 const deviceScaleFactor = 3
 
 const rules: Rule[] = [
-    {
-        viewport: {
-            width: 512,
-            height: 1, // fit to page height
-            deviceScaleFactor,
-        },
-        url: "https://yuruverse2025.mono0x.net/table",
+  {
+    viewport: {
+      width: 512,
+      height: 1, // fit to page height
+      deviceScaleFactor,
     },
-    {
-        viewport: {
-            width: 512,
-            height: 512,
-            deviceScaleFactor,
-        },
-        url: "https://yuruverse2025.mono0x.net/chart",
+    url: "https://yuruverse2025.mono0x.net/table",
+  },
+  {
+    viewport: {
+      width: 512,
+      height: 512,
+      deviceScaleFactor,
     },
+    url: "https://yuruverse2025.mono0x.net/chart",
+  },
 ]
 
 const { values } = util.parseArgs({
-    options: {
-        export: {
-            type: 'boolean',
-            default: false,
-        },
+  options: {
+    export: {
+      type: "boolean",
+      default: false,
     },
+  },
 })
 
 const { export: exportMode } = values
 
-const takeScreenshot = async (page: Page, rule: Rule, name: string): Promise<string> => {
-    await page.setViewport(rule.viewport)
-    await page.goto(rule.url, { waitUntil: "networkidle0" })
-    await page.screenshot({ path: `${name}.png`, fullPage: true })
-    return `${name}.png`
+const takeScreenshot = async (
+  page: Page,
+  rule: Rule,
+  name: string,
+): Promise<string> => {
+  await page.setViewport(rule.viewport)
+  await page.goto(rule.url, { waitUntil: "networkidle0" })
+  await page.screenshot({ path: `${name}.png`, fullPage: true })
+  return `${name}.png`
 }
 
 const post = async (page: Page) => {
-    const {
-        TWITTER_APP_KEY: appKey,
-        TWITTER_APP_SECRET: appSecret,
-        TWITTER_ACCESS_TOKEN: accessToken,
-        TWITTER_ACCESS_SECRET: accessSecret,
-    } = process.env
-    if (appKey == null || appSecret == null || accessToken == null || accessSecret == null) {
-        throw new Error("Twitter API credentials are not set in environment variables.")
-    }
-    const client = new TwitterApi({
-        appKey,
-        appSecret,
-        accessToken,
-        accessSecret,
-    })
+  const {
+    TWITTER_APP_KEY: appKey,
+    TWITTER_APP_SECRET: appSecret,
+    TWITTER_ACCESS_TOKEN: accessToken,
+    TWITTER_ACCESS_SECRET: accessSecret,
+  } = process.env
+  if (
+    appKey == null ||
+    appSecret == null ||
+    accessToken == null ||
+    accessSecret == null
+  ) {
+    throw new Error(
+      "Twitter API credentials are not set in environment variables.",
+    )
+  }
+  const client = new TwitterApi({
+    appKey,
+    appSecret,
+    accessToken,
+    accessSecret,
+  })
 
-    const mediaIds: string[] = []
-    for (const [i, rule] of Object.entries(rules)) {
-        const p = await takeScreenshot(page, rule, path.join(os.tmpdir(), `${i}`))
-        const mediaId = await client.v1.uploadMedia(p)
-        mediaIds.push(mediaId)
-    }
+  const mediaIds: string[] = []
+  for (const [i, rule] of Object.entries(rules)) {
+    const p = await takeScreenshot(page, rule, path.join(os.tmpdir(), `${i}`))
+    const mediaId = await client.v1.uploadMedia(p)
+    mediaIds.push(mediaId)
+  }
 
-    if (!((mediaIds: string[]): mediaIds is [string, string] => {
-        return mediaIds.length == 2;
-    })(mediaIds)) {
-        throw new Error("Too many media files, Twitter allows a maximum of 4 media per tweet.");
-    }
+  if (
+    !((mediaIds: string[]): mediaIds is [string, string] => {
+      return mediaIds.length === 2
+    })(mediaIds)
+  ) {
+    throw new Error(
+      "Too many media files, Twitter allows a maximum of 4 media per tweet.",
+    )
+  }
 
-    const date = format(new Date(), "M月d日", { timeZone: "Asia/Tokyo" })
-    const status = `#ゆるバース2025 ${date}現在のランキング\n\n詳細な投票状況はこちら: https://yuruverse2025.mono0x.net/\n投票はこちら: https://www.yurugp.jp/vote/2025/`
+  const date = format(new Date(), "M月d日", { timeZone: "Asia/Tokyo" })
+  const status = `#ゆるバース2025 ${date}現在のランキング\n\n詳細な投票状況はこちら: https://yuruverse2025.mono0x.net/\n投票はこちら: https://www.yurugp.jp/vote/2025/`
 
-    await client.v2.tweet({
-        text: status,
-        media: { media_ids: mediaIds }
-    })
-};
+  await client.v2.tweet({
+    text: status,
+    media: { media_ids: mediaIds },
+  })
+}
 
 const exportScreenshots = async (page: Page) => {
-    for (const [i, rule] of Object.entries(rules)) {
-        await takeScreenshot(page, rule, `screenshot_${i}`)
-    }
+  for (const [i, rule] of Object.entries(rules)) {
+    await takeScreenshot(page, rule, `screenshot_${i}`)
+  }
 }
 
 const browser = await puppeteer.launch({
-    args: ["--lang=ja"],
+  args: ["--lang=ja"],
 })
 try {
-    const page = await browser.newPage()
-    if (exportMode) {
-        await exportScreenshots(page)
-    } else {
-        await post(page)
-    }
+  const page = await browser.newPage()
+  if (exportMode) {
+    await exportScreenshots(page)
+  } else {
+    await post(page)
+  }
 } finally {
-    await browser.close()
+  await browser.close()
 }
